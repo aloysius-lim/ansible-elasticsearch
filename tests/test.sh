020de4c25d43d226e46e32957d0630832e4fd049
Aloysius Lim <aloysius@abtpeople.com>
2015-05-21 13:23:09 +0800

    Fix test script.

--- tests/test.sh
+++ tests/test.sh
@@ -7,7 +7,7 @@ ansible-playbook --connection=local --syntax-check -vv $DIR/test.yml
 # Check role
 ansible-playbook --connection=local --become -vv $DIR/test.yml
 # Check indempotence
-ansible-playbook --connection=local --become -vv $DIR/test.yml
-  | grep -q 'changed=0.*failed=0'
-  && (echo 'Idempotence test: pass' && exit 0)
+ansible-playbook --connection=local --become -vv $DIR/test.yml \
+  | grep -q 'changed=0.*failed=0' \
+  && (echo 'Idempotence test: pass' && exit 0) \
   || (echo 'Idempotence test: fail' && exit 1)
