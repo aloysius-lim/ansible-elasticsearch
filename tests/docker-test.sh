6d1be8a1488ee7453b0fab68862324cb0b4b77e2
Aloysius Lim <aloysius@abtpeople.com>
2015-05-21 12:31:49 +0800

    Fix CircleCI script.

--- tests/docker-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR=$( dirname $0 )
-ARG=$1
-DOCKERFILE=${ARG#tests/}
-PLATFORM=${DOCKERFILE%.Dockerfile}
-docker build -f $DIR/$DOCKERFILE -t $PLATFORM . && docker run --name $PLATFORM $PLATFORM
